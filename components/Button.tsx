c6aad0ed7aacd5bdfea2ae3e1f41c59062aa328d
Abel Preciado <20259853+IgnisHyper@users.noreply.github.com>
2026-02-08 13:10:55 -0800

    example changes

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({children, onClick, type = "button"}: ButtonProps
     return (
         <button
             type={type}
-            className="px-5 py-2.5 bg-blue-600 text-white font-medium rounded-lg shadow 
+            className="px-5 py-2.5 bg-red-600 text-white font-medium rounded-lg shadow 
             hover:bg-blue-700 active:bg-blue-800 
             focus:outline-none focus:ring-2 focus:ring-blue-300
             transition-all duration-150"
